40945f9e180e55e468050f1e6db1923f36b7a2c1
Alekuyz <axelagia@gmail.com>
2022-03-16 14:11:31 +0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Data.SqlClient;
+
+namespace ActKoneksi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            new Program().Connecting();
+        }
+
+        public void Connecting()
+        {
+            using (
+                SqlConnection con = new SqlConnection("data source=NITRO-AN715;Database=ProdiTI;User ID=sa;Password=123")
+                )
+
+            {
+                con.Open();
+                Console.WriteLine("Nerhasil Terhubung ke Database");
+            }
+        }
+
+    }
+}
